d4509c987623715db924b7d6ba26f6ea94ffad46
Justine Benevent <justinebnvent@gmail.com>
2020-05-22 12:41:59 +0300

    bg size update

--- oh-no-dino.js
+++ oh-no-dino.js
@@ -23,7 +23,7 @@ let eatSound = new Audio('eat.mp3');
 
 function clearCanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(bg, 0, 0);
+    ctx.drawImage(bg, 0, 0, canvas.width, canvas.height);
 }
 
 
